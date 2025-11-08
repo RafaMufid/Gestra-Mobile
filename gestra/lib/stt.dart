70f72aa407cd11209f9760092f4802dc51e2842c
shintaaulya <shintaaulya20@gmail.com>
2025-11-08 17:53:52 +0700

    edit stt

---
+++ gestra/lib/stt.dart
@@ -0,0 +1,164 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class SpeechToTextPage extends StatefulWidget {
+  @override
+  State<SpeechToTextPage> createState() => _SpeechToTextPageState();
+}
+
+class _SpeechToTextPageState extends State<SpeechToTextPage> {
+  bool isRecording = false;
+  late stt.SpeechToText _speech;
+  String recognizedText = "";
+  Timer? _timer;
+  int _seconds = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  String _formatTime(int seconds) {
+    final minutes = (seconds ~/ 60).toString().padLeft(2, '0');
+    final secs = (seconds % 60).toString().padLeft(2, '0');
+    return "$minutes:$secs";
+  }
+
+  void _startTimer() {
+    _seconds = 0;
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _seconds++;
+      });
+    });
+  }
+
+  void _stopTimer() {
+    _timer?.cancel();
+  }
+
+  Future<void> _toggleRecording() async {
+    if (!isRecording) {
+      bool available = await _speech.initialize();
+      if (available) {
+        setState(() {
+          isRecording = true;
+          recognizedText = "";
+        });
+        _startTimer();
+        _speech.listen(onResult: (result) {
+          setState(() {
+            recognizedText = result.recognizedWords;
+          });
+        });
+      }
+    } else {
+      setState(() => isRecording = false);
+      _speech.stop();
+      _stopTimer();
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Speech to Text'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 1,
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  // Bagian timer
+                  Container(
+                    height: 150,
+                    color: Colors.grey[200],
+                    child: Center(
+                      child: Text(
+                        _formatTime(_seconds),
+                        style: const TextStyle(
+                          fontSize: 24,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Hasil teks dari suara
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black26),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        recognizedText.isEmpty
+                            ? "Hasil rekaman suara akan muncul di sini..."
+                            : recognizedText,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+
+                  // Tombol mic
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 40.0),
+                    child: GestureDetector(
+                      onTap: _toggleRecording,
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: isRecording ? Colors.white : Colors.red,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.grey.shade400,
+                            width: 2,
+                          ),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 4,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Icon(
+                            isRecording ? Icons.stop : Icons.mic,
+                            color: isRecording ? Colors.red : Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
