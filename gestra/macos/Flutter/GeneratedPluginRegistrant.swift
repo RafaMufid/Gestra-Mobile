482acb3ce9a97e7c60836553f3e01fc7f26c76d5
Yappier Albertus <yappier.albert1@gmail.com>
2025-12-08 10:57:36 +0700

    backend cihuyyy
    aduhai peningnya

--- gestra/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gestra/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,5 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 }
